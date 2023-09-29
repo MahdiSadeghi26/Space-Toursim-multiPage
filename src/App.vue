8b29eb1ba64d4235b16ff8c09df9c967feeeb211
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-09-29 13:13:46 +0330

    adding home content to flow

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import NavMenu from '@/components/NavMenu.vue';
-
+import HomeContent from '@/components/HomeContent.vue';
 </script>
 <template>
     <div
-        class="h-screen lg:bg-[url('/images/home/background-home-desktop.jpg')] bg-[url('/images/home/background-home-mobile.jpg')] md:bg-[url('/images/home/background-home-tablet.jpg')] bg-fixed bg-center bg-no-repeat bg-cover py-5">
+        class=" overflow-hidden h-screen lg:bg-[url('/images/home/background-home-desktop.jpg')] bg-[url('/images/home/background-home-mobile.jpg')] md:bg-[url('/images/home/background-home-tablet.jpg')] bg-fixed bg-center bg-no-repeat bg-cover py-5">
+        <!-- nav menu -->
         <NavMenu></NavMenu>
+        <!-- contents in center of page -->
+        <HomeContent></HomeContent>
     </div>
 </template>
