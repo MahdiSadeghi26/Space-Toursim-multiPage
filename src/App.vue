7dd3cffa00e332e2f35e3ee1cdb2427e8d36bde9
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-09-29 11:51:49 +0330

    adding navmenu cmp and backgrounds

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import NavMenu from '@/components/NavMenu.vue';
 
 </script>
 <template>
-  <div>
-
-  </div>
+    <div
+        class="h-screen lg:bg-[url('/images/home/background-home-desktop.jpg')] bg-[url('/images/home/background-home-mobile.jpg')] md:bg-[url('/images/home/background-home-tablet.jpg')] bg-fixed bg-center bg-no-repeat bg-cover py-5">
+        <NavMenu></NavMenu>
+    </div>
 </template>
