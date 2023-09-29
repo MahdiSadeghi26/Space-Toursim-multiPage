e8d5b1fa0c576cbc84b2382ae092ce7d99a9ee89
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-09-29 13:13:27 +0330

    left content is ready!

---
+++ src/components/HomeContent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-full  xl:h-[88%] flex flex-col">
+        <!-- title and description -->
+        <div class="px-2 h-full w-1/2 flex item-center  justify-center ">
+            <div class="flex flex-col   items-center justify-center">
+                <p class=" text-lightPurple uppercase font-BarlowRegular text-3xl tracking-widest w-1/2 xl:mb-5  ">so, you
+                    want travel to
+                </p>
+                <!-- title -->
+                <p class="uppercase text-white font-BellefairRegular lg:text-9xl  w-1/2">space</p>
+                <!-- descripiton -->
+                <p class="text-lightPurple font-BarlowLight  text-justify w-1/2 text-xl mt-10">Let’s face it; if you want to
+                    go to space,
+                    you might as well genuinely go to outer space and not hover kind
+                    of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world
+                    experience!
+                </p>
+            </div>
+        </div>
+        <div class="w-1/2"></div>
+    </div>
+</template>
