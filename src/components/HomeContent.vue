574a7f3f1acd8430142eaddc7257c8cea00cb737
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-09-29 16:30:47 +0330

    tablet size is applyed

--- src/components/HomeContent.vue
+++ src/components/HomeContent.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="w-full  xl:h-[88%] flex flex-col">
+    <div class="w-full  lg:h-[88%] flex lg:flex-row flex-col mt-10 lg:mt-auto items-center ">
         <!-- title and description -->
-        <div class="px-2 h-full w-1/2 flex item-center  justify-center ">
-            <div class="flex flex-col   items-center justify-center">
-                <p class=" text-lightPurple uppercase font-BarlowRegular text-3xl tracking-widest w-1/2 xl:mb-5  ">so, you
+        <div class="px-2  w-1/2 h-full flex item-center  justify-center ">
+            <!-- secondary  -->
+            <div class="flex flex-col   items-center justify-center w-full">
+                <p class=" text-lightPurple uppercase font-BarlowRegular text-3xl tracking-widest xl:w-1/2 mb-5 lg:w-2/3  ">
+                    so, you
                     want travel to
                 </p>
                 <!-- title -->
-                <p class="uppercase text-white font-BellefairRegular lg:text-9xl  w-1/2">space</p>
+                <p class="uppercase text-white font-BellefairRegular xl:text-9xl md:text-8xl  xl:w-1/2 lg:w-2/3 ">space</p>
                 <!-- descripiton -->
-                <p class="text-lightPurple font-BarlowLight  text-justify w-1/2 text-xl mt-10">Let’s face it; if you want to
+                <p class="text-lightPurple font-BarlowLight  text-justify xl:w-1/2 lg:w-2/3 text-xl mt-10">Let’s face it; if
+                    you want to
                     go to space,
                     you might as well genuinely go to outer space and not hover kind
                     of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world
@@ -17,6 +20,12 @@
                 </p>
             </div>
         </div>
-        <div class="w-1/2"></div>
+        <!-- explore button -->
+        <div class="w-1/2 flex lg:items-center justify-center md:items-center lg:h-auto h-[350px] ">
+            <div
+                class="bg-white rounded-full xl:w-[250px] xl:h-[250px] lg:w-[200px] lg:h-[200px] md:w-[170px] md:h-[170px] flex items-center justify-center outline-0 outline-gray-400 hover:outline-[40px] outline transition-all hover:duration-500">
+                <a href="" class=" uppercase lg:text-4xl md:text-3xl font-BellefairRegular ">explore</a>
+            </div>
+        </div>
     </div>
 </template>
