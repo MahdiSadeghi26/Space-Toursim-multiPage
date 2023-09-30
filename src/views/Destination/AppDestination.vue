8c7a2bc19095cf22acf505706bce683e163a14cc
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-09-30 19:58:35 +0330

    components path changed to views folder

---
+++ src/views/Destination/AppDestination.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavMenu from '@/components/NavMenu.vue'
+</script>
+<template>
+    <div
+        class="lg:bg-[url('/images/destination/background-destination-desktop.jpg')] md:bg-[url('/images/destination/background-destination-tablet.jpg')] sm:bg-[url('/images/destination/background-destination-mobile.jpg')] h-screen bg-fixed bg-center bg-no-repeat bg-cover py-5">
+        <NavMenu></NavMenu>
+    
+    </div>
+</template>
+
+
+
+
---
+++ src/views/Home/HomeApp.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HomeContent from './HomeContent.vue';
+import NavMenu from '@/components/NavMenu.vue'
+</script>
+
+<template>
+    <div
+        class=" overflow-hidden h-screen lg:bg-[url('/images/home/background-home-desktop.jpg')] bg-[url('/images/home/background-home-mobile.jpg')] md:bg-[url('/images/home/background-home-tablet.jpg')] bg-fixed bg-center bg-no-repeat bg-cover py-5">
+        <NavMenu></NavMenu>
+        <HomeContent></HomeContent>
+    </div>
+</template>
