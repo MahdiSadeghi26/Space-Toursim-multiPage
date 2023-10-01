bb000e699c53360d95aaa0c642b633497c345df5
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-10-01 18:50:54 +0330

    removeing background from app and added to home cmpp

--- src/views/Home/HomeApp.vue
+++ src/views/Home/HomeApp.vue
@@ -5,7 +5,7 @@ import NavMenu from '@/components/NavMenu.vue'
 
 <template>
     <div
-        class=" overflow-hidden h-screen lg:bg-[url('/images/home/background-home-desktop.jpg')] bg-[url('/images/home/background-home-mobile.jpg')] md:bg-[url('/images/home/background-home-tablet.jpg')] bg-fixed bg-center bg-no-repeat bg-cover py-5">
+        class="overflow-hidden h-screen lg:bg-[url('/images/home/background-home-desktop.jpg')] bg-[url('/images/home/background-home-mobile.jpg')] md:bg-[url('/images/home/background-home-tablet.jpg')] bg-fixed bg-center bg-no-repeat bg-cover">
         <NavMenu></NavMenu>
         <HomeContent></HomeContent>
     </div>
