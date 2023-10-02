34b9d8fb19dd84353405a979825231ff3a98aae2
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-10-02 20:04:35 +0330

    animation added

--- src/views/Home/HomeContent.vue
+++ src/views/Home/HomeContent.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="w-full  lg:h-[88%] flex lg:flex-row flex-col mt-10 lg:mt-auto items-center ">
         <!-- title and description -->
-        <div class="px-2  sm:w-1/2 h-full flex item-center  justify-center ">
-            <!-- secondary  -->
-            <div class="flex flex-col   items-center justify-center w-full">
-                <p
+        <transition appear enter-active-class="animate__animated animate__fadeIn">
+        
+            <div class="px-2  sm:w-1/2 h-full flex item-center  justify-center ">
+                <!-- secondary  -->
+                <div class="flex flex-col   items-center justify-center w-full">
+                    <p
                     class=" text-lightPurple uppercase font-BarlowRegular lg:text-3xl sm:text-2xl text-xl tracking-widest xl:w-1/2 mb-5 lg:w-2/3  ">
                     so, you
                     want travel to
@@ -13,8 +15,9 @@
                 <p
                     class="uppercase text-white font-BellefairRegular xl:text-9xl md:text-8xl sm:text-7xl text-5xl  xl:w-1/2 lg:w-2/3 ">
                     space</p>
-                <!-- descripiton -->
-                <p class="text-lightPurple font-BarlowLight  text-justify xl:w-1/2 lg:w-2/3 text-xl mt-10 w-11/12">Let’s face it; if
+                    <!-- descripiton -->
+                    <p class="text-lightPurple font-BarlowLight  text-justify xl:w-1/2 lg:w-2/3 text-xl mt-10 w-11/12">Let’s
+                    face it; if
                     you want to
                     go to space,
                     you might as well genuinely go to outer space and not hover kind
@@ -23,12 +26,20 @@
                 </p>
             </div>
         </div>
+    </transition>
         <!-- explore button -->
+
+
         <div class="w-1/2 flex lg:items-center justify-center items-center lg:h-auto md:h-[350px] h-[250px] ">
-            
+            <transition appear enter-active-class="animate__animated animate__fadeIn">
                 <router-link class=" uppercase lg:text-4xl md:text-3xl font-BellefairRegular bg-white rounded-full xl:w-[250px] xl:h-[250px] lg:w-[200px] lg:h-[200px]
-                 md:w-[170px] md:h-[170px] w-[150px] h-[150px] flex items-center justify-center outline-0 outline-gray-400 hover:outline-[40px] 
-                 outline transition-all hover:duration-500" :to="{name : 'destination'}">explore</router-link>
-           
+    md:w-[170px] md:h-[170px] w-[150px] h-[150px] flex items-center justify-center outline-0 outline-gray-400 hover:outline-[40px] 
+    outline transition-all hover:duration-500" :to="{ name: 'destination' }">explore</router-link>
+
+
+
+            </transition>
         </div>
-</div></template>
+
+    </div>
+</template>
