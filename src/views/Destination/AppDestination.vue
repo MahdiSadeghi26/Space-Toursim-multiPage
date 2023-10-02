9687155b7650cd98220c24ced110c68ecbbc3749
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-10-02 20:05:03 +0330

    height added for small size

--- src/views/Destination/AppDestination.vue
+++ src/views/Destination/AppDestination.vue
@@ -4,7 +4,7 @@ import DestenitonContent from './DestenitonContent.vue';
 </script>
 <template>
     <div
-        class="lg:bg-[url('/images/destination/background-destination-desktop.jpg')] md:bg-[url('/images/destination/background-destination-tablet.jpg')] bg-[url('/images/destination/background-destination-mobile.jpg')] lg:h-screen md:h-[1080px]   bg-fixed bg-center lg:bg-no-repeat  bg-cover ">
+        class="lg:bg-[url('/images/destination/background-destination-desktop.jpg')] md:bg-[url('/images/destination/background-destination-tablet.jpg')] bg-[url('/images/destination/background-destination-mobile.jpg')] lg:h-screen md:h-[1080px] h-[1050px]  bg-fixed bg-center lg:bg-no-repeat  bg-cover ">
         <DestenitonContent class="self-center">
         </DestenitonContent>
 
