27d4ef2f50f239418a14ca692bf1221bb14d8459
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-10-09 16:22:36 +0330

    technology cmp bg and nav menu added

--- src/views/Technology/TechnologyApp.vue
+++ src/views/Technology/TechnologyApp.vue
@@ -1,5 +1,11 @@
+<script setup>
+import NavMenu from '@/components/NavMenu.vue'
+</script>
+
 <template>
-    <div>
-        
+    <div
+        class="lg:bg-[url('/images/technology/background-technology-desktop.jpg')] lg:bg-no-repeat md:bg-[url('/images/technology/background-technology-tablet.jpg')] bg-no-repeat bg-[url('/images/technology/background-technology-mobile.jpg')] h-screen bg-center bg-cover ">
+        <NavMenu></NavMenu>
     </div>
 </template>
+
