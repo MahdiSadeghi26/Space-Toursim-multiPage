6464a55ea29447af824c788e4edeb2241a7670a8
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-10-11 19:30:19 +0330

    fixed relative address

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeApp from '/views/Home/HomeApp.vue';
-import AppDestination from '/views/Destination/AppDestination.vue'
-import CrewApp from '/views/Crew/CrewApp.vue'
-import TechnologyApp from '/views/Technology/TechnologyApp.vue'
+import HomeApp from '@/views/Home/HomeApp.vue';
+import AppDestination from '@/views/Destination/AppDestination.vue'
+import CrewApp from '@/views/Crew/CrewApp.vue'
+import TechnologyApp from '@/views/Technology/TechnologyApp.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
